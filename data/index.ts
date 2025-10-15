db4501d73ca75406059343adf9dbf8168a3c696b
Karthik J <114949890+Karthik150502@users.noreply.github.com>
2025-10-15 16:25:19 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -65,7 +65,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building Debate.ii",
+    title: "Currently building Debate.io",
     description: "A Video Conferencing Application for debates, with integrated AI chatbot for fact checking.",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
